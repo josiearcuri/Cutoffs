8d5ea4b3f1745552535d83e5998a7bd3296819f5
josiearcuri <jarcuri@indiana.edu>
2020-10-26 22:07:02 -0400

    fix constants

--- SimulateCutoffDistribution.py
+++ SimulateCutoffDistribution.py
@@ -9,7 +9,7 @@ import meanderpyalt as mp
 
 #set variables
 D = 10;                       
-nit = 50000                   # number of iterations, run for at least 5000 years to see clustering
+nit = 5000                   # number of iterations, run for at least 5000 years to see clustering
 Cf = 0.022                # dimensionless Chezy friction factor
 kl = 5/(365*24*60*60.0)   # migration rate constant (m/s)
 kv =  1.0E-11              # vertical slope-dependent erosion rate constant (m/s)
@@ -17,7 +17,7 @@ dt = 1*365*24*60*60.0     # time step (s)
 dens = 100                  # density of water (kg/m3)
 saved_ts = 1               # which time steps will be saved every year
 decay_rate = 1/10;         #ranges between 1/3 to 1/10, eventually this will not be a constant
-bump_scale = 1.5            #to multiple kl by, range between 1 and 3, set to 0 for no nonlocal effects
+bump_scale = 1.5           #to multiple kl by, range between 1 and 3, set to 0 for no nonlocal effects
 Sl = 0.01                    # initial slope (matters more for submarine channels than rivers)
 pad= 20                     #depends on sample
 
