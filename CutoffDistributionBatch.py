47a9866f78120273c4067602fb75e6eb99075749
josiearcuri <jarcuri@indiana.edu>
2020-10-28 08:56:46 -0400

    dt

--- CutoffDistributionBatch.py
+++ CutoffDistributionBatch.py
@@ -9,19 +9,19 @@ import os
 
 #set variables
 D = 10;                       
-nit = 200                   # number of iterations, run for at least 5000 years to see clustering
+nit = 100                   # number of iterations, run for at least 5000 years to see clustering
 Cf = 0.022                # dimensionless Chezy friction factor
 kl = 5/(365*24*60*60.0)   # migration rate constant (m/s)
 kv =  1.0E-11              # vertical slope-dependent erosion rate constant (m/s)
-dt = .5*365*24*60*60.0     # time step (s)
+dt = 365*24*60*60.0     # time step (s)
 dens = 100                  # density of water (kg/m3)
-saved_ts = 2               # which time steps will be saved every year
+saved_ts = 1               # which time steps will be saved every year
 decay_rate = 1;         #ranges between 1/3 to 1/10, eventually this will not be a constant
 bump_scale = 0           #to multiple kl by, range between 1 and 3, set to 0 for no nonlocal effects
 Sl = 0.01                    # initial slope (matters more for submarine channels than rivers)
 pad= 100                     #depends on sample
 
-result_dir = "C:/Users/Josie/Desktop/dump_justcurvature/" ##change this to wherevery you want to save your results
+result_dir = "C:/Users/Josie/Desktop/dump_justcurvature3/" ##change this to wherevery you want to save your results
 filelist = ['sample_data/Reach6CL1984.csv','sample_data/Reach6CL_widths1984.csv']
 
 #Simulate migration on real centerline, keeoing track of cutoff locationa nd times#
