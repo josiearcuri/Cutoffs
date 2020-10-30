a91b2f0fbd7cb8f480c53c674ba98f1dcd2f9ac3
josiearcuri <jarcuri@indiana.edu>
2020-10-29 20:30:26 -0400

    retry

--- testclustering.py
+++ testclustering.py
@@ -4,5 +4,6 @@ import pandas as pd
 file = "C:/Users/Josie/Desktop/dump_nonlocaleffects_pad20/9000years_cutoff_distributions.csv"
 cutoffs = pd.read_csv(file, sep = ',')
 print(cutoffs.head())
-
-#co.mc_envelope(cutoffs, year, resultdir, nit = 99, mode = ' modeled')
+year = np.max(cutoffs.time)
+resultdir = "C:/Users/Josie/Desktop/"
+co.mc_envelope(cutoffs, year, resultdir, nit = 99, mode = ' modeled')
