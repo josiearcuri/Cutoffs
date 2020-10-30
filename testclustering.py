6d80209fea9f886acc23f5491db369d8be4e5670
josiearcuri <jarcuri@indiana.edu>
2020-10-29 20:39:48 -0400

    smaller file

--- testclustering.py
+++ testclustering.py
@@ -2,7 +2,7 @@ import cutoffs as co
 import pandas as pd
 import numpy as np
 
-file = "C:/Users/Josie/Desktop/dump_nonlocaleffects_pad20/6000years_cutoff_distributions.csv"
+file = "C:/Users/Josie/Desktop/dump_nonlocaleffects_pad20/2000years_cutoff_distributions.csv"
 cutoffs = pd.read_csv(file, sep = ',')
 print(cutoffs.head())
 year = np.max(cutoffs.time)
