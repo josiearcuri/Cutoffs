8ef8ba1e7f4633a1dbc5f02f34c5ce23e568a1cd
josiearcuri <jarcuri@indiana.edu>
2020-10-29 20:26:06 -0400

    test clustering detections method

---
+++ testclustering.py
@@ -0,0 +1,8 @@
+import cutoffs as co
+import pandas as pd
+
+file = "C:/Users/Josie/Desktop/dump_nonlocaleffects_pad20/9000years_cutoff_distributions.csv"
+cutoffs = pd.read_csv(file, sep = ',')
+print(cutoffs.head())
+
+#co.mc_envelope(cutoffs, year, resultdir, nit = 99, mode = ' modeled')
