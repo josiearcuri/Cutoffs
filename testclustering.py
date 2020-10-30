a4499a2d8172cd77b460f3d18fb6427e02c2c8d3
josiearcuri <jarcuri@indiana.edu>
2020-10-30 12:31:24 -0400

    significant change

--- testclustering.py
+++ testclustering.py
@@ -2,9 +2,9 @@ import cutoffs as co
 import pandas as pd
 import numpy as np
 
-file = "C:/Users/Josie/Desktop/dump_nonlocaleffects_pad20/2000years_cutoff_distributions.csv"
+file = "C:/Users/Josie/Desktop/dump_nonlocaleffects_pad20/5000years_cutoff_distributions.csv"
 cutoffs = pd.read_csv(file, sep = ',')
-print(cutoffs.head())
-year = np.max(cutoffs.time)
+
+year = 5000
 resultdir = "C:/Users/Josie/Desktop/"
-co.mc_envelope(cutoffs, year, resultdir, nit = 99, mode = ' modeled')
+co.mc_envelope(cutoffs, year, resultdir, nit = 99, mode = ' with nonlocal effects')
