5e8081bd2f63d956b7ba93608daa20a368a75090
josiearcuri <jarcuri@indiana.edu>
2020-12-01 12:01:23 -0500

    get rid of old sample results

--- preliminarytests.py
+++ preliminarytests.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from SpaceTime import PreliminaryTesting
 
-file = "C:/Users/Josie/Desktop/Cutoffs/sample_results/InitialChannel/OnlyCurvature_new/1000years_cutoff_distributions_oc.csv"
+file = "C:/Users/Josie/Desktop/Cutoffs/sample_results/InitialChannel/OnlyCurvature_new/2000years_cutoff_distributions.csv"
 cutoffs = pd.read_csv(file, sep = ',')
 year = 2000
 print(np.max(cutoffs['downstream_distance']))
