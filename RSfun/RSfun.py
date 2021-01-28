f7751735c4322990cce698f872a2455581229acc
josiearcuri <jarcuri@indiana.edu>
2021-01-28 14:52:58 -0500

    probability functions

--- RSfun/RSfun.py
+++ RSfun/RSfun.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+from scipy.stats import norm
 
 def merge_dfs(cutoff_file, reach_file):
     """
@@ -59,6 +61,60 @@ def near_neigh(cutoffs_df):
             cutoffs_df.loc[row,'near_neigh_time'] = nn_time[count]
     return cutoffs_df
 
+def probs(cutoffs_df):
+    N_points = len(cutoffs_df.index)
+    n_bins_bump = 5
+    n_bins_time = 10
+
+    # Generate a normal distribution, center at x=0 and y=5
+    time = cutoffs_df['duration'].values#
+    bump = cutoffs_df['bump'].values
+    
+    median_time = np.mean(time)
+    std_time = np.std(time)
+ 
+    median_bump =np.mean(bump)
+    std_bump = np.std(bump)
+  
+    fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+    time_sig_points = np.arange(median_time-(2*std_time), median_time+(2*std_time), 1)
+    bump_sig_points = np.arange(median_bump-(2*std_bump), median_time+(2*std_bump), 1)
+    N, bins_time, patches_time = axs[0].hist(time, range = (0,10),bins=n_bins_time, density=True, label = 'n = '+str(N_points), histtype = 'bar', facecolor = 'lightgrey', edgecolor = 'k')
+    N, bins_bump, patches_bump =axs[1].hist(bump, range = (0,5), bins=n_bins_bump, density = True,  label = 'n = '+str(N_points), histtype = 'bar', facecolor = 'lightgrey', edgecolor = 'k')
+    # We can set the number of bins with the `bins` kwarg
+    N, bins_time, patches_time = axs[0].hist(time, range = (0,10),bins=n_bins_time, density=True, label = "95% CI", histtype = 'bar', facecolor = 'r', edgecolor = 'k')
+    N, bins_bump, patches_bump =axs[1].hist(bump, range = (0,5), bins=n_bins_bump, density = True,  label = "95% CI", histtype = 'bar', facecolor = 'r', edgecolor = 'k')
+
+    axs[0].legend(prop={'size': 10}, frameon = False)
+    axs[1].legend(prop={'size': 10}, frameon = False)
+    
+    for p,b in zip(patches_time,bins_time[:-1]):
+        if b-2<(median_time-(2*std_time)) or b>=(median_time+(2*std_time)):
+            p.set_facecolor('lightgrey')
+    for p,b in zip(patches_bump,bins_bump[:-1]):
+        if b-2<(median_bump-(2*std_bump)) or b>=(median_bump+(2*std_bump)):
+            p.set_facecolor('lightgrey')
+            
+
+
+    
+    axs[0].yaxis.set_major_formatter(PercentFormatter(xmax=1))
+    
+    axs[0].set_xlabel('duration [years]')
+    axs[1].set_xlabel('bump [max(MR-BR)/BR]')
+
+    axs[0].set_title('P(duration)')
+    axs[1].set_title('P(bump)')
+    
+
+    
+    axs[0].spines['top'].set_visible(False)
+    axs[0].spines['right'].set_visible(False)
+    
+    axs[1].spines['top'].set_visible(False)
+    axs[1].spines['right'].set_visible(False)
+    
+    return fig
 
 def plot_nn_v1(cutoffs_df):
     x_s = np.asarray(cutoffs_df['near_neigh_dist']/cutoffs_df['length'])
--- RSplots.py
+++ RSplots.py
@@ -6,7 +6,7 @@ reach_file = "sample_data/RSCutoffData/fakereaches.csv"
 
 cutoffs_df = rs.merge_dfs(cutoff_file, reach_file)
 cutoffs_df = rs.near_neigh(cutoffs_df)
-rs.plot_nn_v2(cutoffs_df)
- 
 
+
+rs.probs(cutoffs_df)
 plt.show()
